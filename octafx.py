5e3b32ccdc4d0778444a3779976dd9355d327379
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-11-17 01:29:15 +0530

    added method to get trade info for different accounts

--- octafx.py
+++ octafx.py
@@ -2,6 +2,10 @@ import config
 import requests
 from pprint import pprint
 
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+'AppleWebKit/537.36 (KHTML, like Gecko) '
+'Chrome/86.0.4240.198 Safari/537.36'
+
 
 class Octafx:
     def __init__(self):
@@ -14,9 +18,7 @@ class Octafx:
             'accept': 'application/json, text/plain, */*',
             'dnt': '1',
             'x-requested-with': 'XMLHttpRequest',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/86.0.4240.198 Safari/537.36',
+            'user-agent': USER_AGENT,
             'content-type': 'application/json;charset=UTF-8',
             'origin': 'https://my.octafx.com',
             'sec-fetch-site': 'same-origin',
@@ -36,7 +38,32 @@ class Octafx:
         response = self.session.post('https://my.octafx.com/auth/login/', headers=headers, json=data)
         return response.status_code == 200
 
+    def update_trades(self, account_number='13102515'):
+        headers = {
+            'authority': 'www.octafx.com',
+            'upgrade-insecure-requests': '1',
+            'dnt': '1',
+            'user-agent': USER_AGENT,
+            'accept': '*/*',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'referer': 'https://www.octafx.com/copy-trade/copier-area/',
+            'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,sv;q=0.6',
+        }
+
+        response = self.session.get(
+            f'https://www.octafx.com/copy-trade/change-current-account/copier/{account_number}/copy_trade_copier_area/',
+            headers=headers
+        )
+
+        with open('trades.html', 'w', encoding='utf-8') as f:
+            f.write(response.text)
+
 
 if __name__ == '__main__':
+    login = True
     octafx = Octafx()
-    print(octafx.login())
+    print('logged in:', octafx.login())
+    octafx.update_trades(account_number=config.accounts['espada8816'])
